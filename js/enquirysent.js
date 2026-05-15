19a062e82c7f00be59a57f58f9486a8b4d5326ba
Xukia_0 <oscarng926@gmail.com>
2026-05-15 13:45:59 +0800

    added all of our required files

--- js/enquirysent.js
+++ js/enquirysent.js
@@ -1,7 +1,7 @@
 const submit = document.getElementById("submitbutton");
 const success = document.getElementById("submitsuccess");
 
-function submitted(event) { //submitformevent inherited from event
+function submitted(event) { //submitform event inherited from event
     success.innerHTML = "Enquiry successfully sent!";
     event.preventDefault();
 }
